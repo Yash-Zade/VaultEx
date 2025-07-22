d9561821898651ce38306ae18d46301da7e21b9e
Yash Zade <yashmzade@gmail.com>
2025-07-22 21:02:34 +0530

    Create Sell feature in Virtual AMM

--- Contracts/src/core/VirtualAMM.sol
+++ Contracts/src/core/VirtualAMM.sol
@@ -12,8 +12,6 @@ contract VirtualAMM{
     uint256 public k;
     IPriceFeed public priceFeed;
 
-
-
     constructor(uint256 _baseReserve, uint256 _quoteReserve, address _priceFeedAddress) {
         owner = msg.sender;
         baseReserve = _baseReserve;
@@ -22,50 +20,54 @@ contract VirtualAMM{
         k = baseReserve * quoteReserve;
     }
 
-
-
     modifier onlyOwner{
         require(msg.sender == owner, "Not the owner");
         _;
     }
 
-    // function desposit(address _address, uint256 _amount) public onlyOwner{
-
-    // }
-
-    // function withdraw(address _address, uint256 _amount) public onlyOwner{
-
-    // }
-
     function buy(uint256 _collateral, uint256 _size) public returns(uint256){
 
-        uint256 newQuoteReserve = _size;
-        uint256 newBaseReserve = k/quoteReserve;
+        uint256 newQuoteReserve = quoteReserve + _size;
+        uint256 newBaseReserve = k/newQuoteReserve;
 
         quoteReserve = newQuoteReserve;
         baseReserve = newBaseReserve;
 
         lastPrice = getVirtualAMMPrice();
 
-        uint mintableTokens = getMintablevETH(_collateral);
+        uint mintableTokens = getMintableToken(_collateral);
 
         return mintableTokens;
-
     }
 
-    function sell(address _user,uint256 _collateral, uint256 _size, uint256 _entryPrice, uint8 _leverage) public returns(uint256){
-        
+    function sell(uint256 _collateral) public returns(uint256){
+
+        uint256 baseIn = getMintableToken(_collateral);
+        uint256 newBaseReserve = baseReserve + baseIn;
+
+        uint newQuoteReserve = k/newBaseReserve;
+
+        uint256 vUSDTAmount = quoteReserve - newQuoteReserve;
+
+        quoteReserve = newQuoteReserve;
+        baseReserve = newBaseReserve;
+
+        lastPrice = getVirtualAMMPrice();
+
+        return vUSDTAmount;
     }
+
+
     function getVirtualAMMPrice() public view returns (uint256) {
         require(baseReserve > 0, "Invalid reserve");
         return (quoteReserve * 1e18) / baseReserve;
     }
 
-    function getMintablevETH(uint256 usdtAmount) internal view returns (uint256) {
+    function getMintableToken(uint256 usdtAmount) internal view returns (uint256) {
         uint256 ethPrice = getVirtualAMMPrice();
         require(ethPrice > 0, "Invalid price");
 
-        return (usdtAmount * 1e18) / uint256(ethPrice);
+        return (usdtAmount * 1e18) / (ethPrice);
     }
 
 }
